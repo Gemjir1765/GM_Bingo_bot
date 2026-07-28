9218bce879fdc052bd5f6b380f239226ecd213fa
Gemechu Jira <gemechujira1765@gmail.com>
2026-07-29 01:06:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -63,18 +63,18 @@ LOCATION = 3
 #JSON FUNCTIONS
 #==========================
 
-def load_json(filename):
-if not os.path.exists(filename):
-with open(filename, "w", encoding="utf-8") as f:
-json.dump({}, f)
+def ldef load_json(filename):
+    if not os.path.exists(filename):
+        with open(filename, "w", encoding="utf-8") as f:
+            json.dump({}, f)
 
-with open(filename, "r", encoding="utf-8") as f:
-return json.load(f)
+    with open(filename, "r", encoding="utf-8") as f:
+        return json.load(f)
 
 
 def save_json(filename, data):
-with open(filename, "w", encoding="utf-8") as f:
-json.dump(data, f, indent=4, ensure_ascii=False)
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=4, ensure_ascii=False)
 
 
 #==========================
